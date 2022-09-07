a8f0ced0235424dcf29eb72b7455b12caa6ea374
albim72 <marcin.albiniak72@gmail.com>
2022-09-07 11:23:36 +0200

    Create input.py

---
+++ PODSTAWY/input.py
@@ -0,0 +1,17 @@
+#komentarz w Pythonie
+#nowa linia
+"""
+komentarz wieloliniowy - dokumentacyjny
+"""
+"""
+komentarz wieloliniowy - niedokumentacyjny
+"""
+
+print("podaj swoje imię: ")
+imie = input()
+
+print("Miło Cię widzieć",imie)
+print("Liczba znaków imienia: " + str(len(imie)))
+
+wiek = int(input("podaj swój wiek: "))
+print("Twój wiek za 10 lat: " + str(wiek+10))
