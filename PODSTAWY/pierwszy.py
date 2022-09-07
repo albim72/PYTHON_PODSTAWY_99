7ce450e90f87043e24db3617ce1b960c2850d58f
albim72 <marcin.albiniak72@gmail.com>
2022-09-07 11:10:29 +0200

    Update pierwszy.py

--- PODSTAWY/pierwszy.py
+++ PODSTAWY/pierwszy.py
@@ -17,3 +17,37 @@ print(type(d))
 
 d = "blabla"
 print(type(d))
+
+p1 = "pora roku - lato"
+p2 = "pora roku - jesień"
+u = 99
+
+print(p1 + ", " + p2 + str(u))
+print(p1,p2,u,sep=", ")
+
+y = 33.234
+print(7*y)
+
+z = "56"
+print(7*z)
+
+t = "tak"
+print(11*t)
+
+print(7*int(z))
+print(7*eval(z))
+
+g1 = 10
+g2 = 11
+
+print(g1+g2,g1-g2,g1*g2,g1/g2,g1%g2,g1**g2)
+
+print(round(10.52))
+print(round(10.52,1))
+print(pow(5,6))
+print(5**6)
+
+i = 1
+i+=1 
+i = i+1
+print(i)
