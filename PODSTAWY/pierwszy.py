a185b5a1b0789522f4b88360fd13a26bda7e5015
albim72 <marcin.albiniak72@gmail.com>
2022-09-07 10:55:11 +0200

    Create pierwszy.py

---
+++ PODSTAWY/pierwszy.py
@@ -0,0 +1,19 @@
+print("komunikat nr 1: pierwszy program!")
+a = 7
+print(a)
+print(type(a))
+
+a = 11.5
+print(a)
+print(type(a))
+
+a = "jedynka"
+print(a)
+print(type(a))
+
+d:float = 9.9
+
+print(type(d))
+
+d = "blabla"
+print(type(d))
