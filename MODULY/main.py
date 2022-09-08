ac929eb75365ff7154e940196a0b1346ab2ee65a
albim72 <marcin.albiniak72@gmail.com>
2022-09-08 15:40:05 +0200

    Update main.py

--- MODULY/main.py
+++ MODULY/main.py
@@ -4,6 +4,7 @@ from dane import osoba
 from moje_funkcje import witaj,px
 #from miasto import Miasto
 from stolica import Stolica,Miasto
+from dodatki.ekstra_funkcje.mf import mojafunkcja,px_ext
 
 print(osoba)
 print(witaj("Tomasz"))
@@ -16,3 +17,6 @@ ms.print_miasto()
 st = Stolica(1,"Warszawa","mazowieckie","Jan Kot")
 st.print_miasto()
 print(st.prezydentinfo())
+
+print(mojafunkcja(3,7,18,5))
+print(px_ext(4,7,3))
