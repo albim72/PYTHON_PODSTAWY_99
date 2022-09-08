b9d54481226a8fdb898bb24c94cb95372329f9e4
albim72 <marcin.albiniak72@gmail.com>
2022-09-08 13:26:09 +0200

    Update osoby.py

--- OBIEKTY/osoby.py
+++ OBIEKTY/osoby.py
@@ -35,3 +35,26 @@ p2.print_osoba()
 print(p2.wiekza10lat())
 print(f'czy osoba jest pracownikiem? {p2.czypracownik()}')
 
+class Pracownik(Osoba):
+
+    #konstruktor z dziedziczeniem
+    def __init__(self,imie,wiek,waga,wzrost,firma,stanowisko,latapracy,wynagrodzenie):
+        super().__init__(imie,wiek,waga,wzrost)
+        self.firma = firma
+        self.stanowisko = stanowisko
+        self.latapracy = latapracy
+        self.wynagrodzenie = wynagrodzenie
+
+    def print_pracownik(self):
+        print(f"pracownik -> firma: {self.firma}, stanowisko pracy: {self.stanowisko}, lata pracy: {self.latapracy}, "
+              f"wynagroodzenie: {self.wynagrodzenie} zł")
+
+    def czypracownik(self) -> bool:
+        return True
+
+print("______________________________________________________")
+e1 = Pracownik("Karol",47,101,173,"ABC","dyrektor",12,11400)
+e1.print_osoba()
+e1.print_pracownik()
+print(e1.wiekza10lat())
+print(f'czy osoba jest pracownikiem? {e1.czypracownik()}')
