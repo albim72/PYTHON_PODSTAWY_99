35ce94ab452a27adc3e072d3b66751a4fbb34070
albim72 <marcin.albiniak72@gmail.com>
2022-09-08 13:08:41 +0200

    Update osoby.py

--- OBIEKTY/osoby.py
+++ OBIEKTY/osoby.py
@@ -1,5 +1,5 @@
 class Osoba:
-    
+
     def __init__(self,imie:str,wiek:int,waga:float,wzrost:float):
         self.imie = imie
         self.wiek = wiek
@@ -7,16 +7,31 @@ class Osoba:
         self.wzrost = wzrost
         self.kolor_oczu = "brązowe"
         self.info()
-        
+
     def info(self) -> None:
         print("Tworzenie nowej instancji klasy Osoba -> obiekt Osoba")
-        
+
     def print_osoba(self) -> None:
         print(f"osoba -> imię: {self.imie}, wiek: {self.wiek} lat, waga: {self.waga} kg,"
-              f" wzrost: {self.wzrost} cm.")
-        
+              f" wzrost: {self.wzrost} cm, kolor oczu: {self.kolor_oczu}")
+
     def wiekza10lat(self) -> int:
         return self.wiek+10
-    
+
     def czypracownik(self) -> bool:
         return False
+
+
+p1 = Osoba("Jan",45,112,174)
+p1.print_osoba()
+print(p1.wiekza10lat())
+print(f'czy osoba jest pracownikiem? {p1.czypracownik()}')
+
+print("______________________________________________________")
+
+p2 = Osoba("Olga",24,54,168)
+p2.kolor_oczu = "niebieskie"
+p2.print_osoba()
+print(p2.wiekza10lat())
+print(f'czy osoba jest pracownikiem? {p2.czypracownik()}')
+
