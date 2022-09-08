fb45a1f3c42eeb302ce80f266b55a942a11319e0
albim72 <marcin.albiniak72@gmail.com>
2022-09-08 15:02:19 +0200

    Update klasa123.py

--- OBIEKTY/klasa123.py
+++ OBIEKTY/klasa123.py
@@ -1,38 +1,47 @@
 class PierwszaKlasa:
-    
+
     def __init__(self,a,b):
         self.a = a
         self.b = b
-        
+
     def print_ab(self):
         print(f"a = {self.a}, b = {self.b}")
-        
-        
+
+
 class DrugaKlasa(PierwszaKlasa):
-    
+
     def __init__(self,a,b,c):
         super().__init__(a,b)
         self.c = c
-        
+
     def print_abc(self):
         print(f"a = {self.a}, b = {self.b}, c = {self.c}")
-        
+
     def sumuj(self):
         return self.a+self.b+self.c
-    
-class TrzeciaKlasa(DrugaKlasa):
-    
-    def __init__(self,a,b,c,d):
-        super().__init__(a,b,c)
+
+class Dodatkowa:
+
+    def __init__(self,g):
+        self.g = g**2
+
+    def fx(self):
+        return 3*self.g - 2
+
+class TrzeciaKlasa(DrugaKlasa,Dodatkowa):
+
+    def __init__(self,a,b,c,d,g):
+        DrugaKlasa.__init__(self,a,b,c)
+        Dodatkowa.__init__(self,g)
         self.d = d
 
-    def print_abcd(self):
-        print(f"a = {self.a}, b = {self.b}, c = {self.c}, d={self.d}")
+    def print_abcdg(self):
+        print(f"a = {self.a}, b = {self.b}, c = {self.c}, d={self.d}, g = {self.g}")
 
     def sumuj(self):
-        return self.a + self.b + self.c + self.d
+        return self.a + self.b + self.c + self.d + self.fx()
 
-    print("____________klasa pierwsza______________")
+print("____________klasa pierwsza______________")
 pk = PierwszaKlasa(3,6)
 pk.print_ab()
 print("____________klasa druga________________")
@@ -43,8 +52,15 @@ print(f"suma a+b+c = {dk.sumuj()}")
 
 print("____________klasa trzecia_______________")
 
-tk = TrzeciaKlasa(6,17,8,19)
+tk = TrzeciaKlasa(6,17,8,19,5)
 tk.print_ab()
 tk.print_abc()
-tk.print_abcd()
-print(f"suma a+b+c+d = {tk.sumuj()}")
+tk.print_abcdg()
+print(f"suma a+b+c+d+fx(g) = {tk.sumuj()}")
+
+#zbuduj klasę Dodatkowa, konsruktor ma być oparty na parametrze g i ma przkazywać kwadrat zadanego parametru
+# do zmiennej g, zbuduj dla tej klasy metodę fx -> wynik = 3*g-2
+# dodaj do dziedziczenie w TrzeciaKlasa klasę Dodatkowa -> zmień konstruktor z wielodziedziczeniem
+#zmodyfikuj metodę print_abcd na print_abcdg -> wyświetl tam dodtkowo g
+#zmodyfikuj moeodę sumuj tak aby zwracała -> a+b+c+d + fx(g)
+
