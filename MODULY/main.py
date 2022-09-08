55f17fe674065891080a6d93a12ff01998fa862e
albim72 <marcin.albiniak72@gmail.com>
2022-09-08 15:19:58 +0200

    Update main.py

--- MODULY/main.py
+++ MODULY/main.py
@@ -2,8 +2,12 @@
 #import dane as dn
 from dane import osoba
 from moje_funkcje import witaj,px
+from miasto import Miasto
 
 print(osoba)
 print(witaj("Tomasz"))
 
 print(px(4,7))
+
+ms = Miasto(34,"Toruń","kujawsko-pomorskie")
+ms.print_miasto()
