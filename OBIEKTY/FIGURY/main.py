5abbf2c20a4e3cfe8ff07b173d69efb93395e4db
albim72 <marcin.albiniak72@gmail.com>
2022-09-09 11:23:43 +0200

    Create main.py

---
+++ OBIEKTY/FIGURY/main.py
@@ -0,0 +1,8 @@
+from prostokat import Prostokat
+from trojkat import Trojkat
+
+pr = Prostokat(4.5,6.3)
+print(f"pole prostokąta wynosi: {pr.policzpole():.2f} cm2")
+
+tr = Trojkat(4.6,6.2)
+print(f"pole trójkąta wynosi: {tr.policzpole():.2f} cm2")
