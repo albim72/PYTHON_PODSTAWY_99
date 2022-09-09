2569e20a1bd3bc3a05375556d6373680c22e9d7c
albim72 <marcin.albiniak72@gmail.com>
2022-09-09 11:28:34 +0200

    Update main.py

--- OBIEKTY/FIGURY/main.py
+++ OBIEKTY/FIGURY/main.py
@@ -1,8 +1,12 @@
 from prostokat import Prostokat
 from trojkat import Trojkat
+from trapez import Trapez
 
 pr = Prostokat(4.5,6.3)
 print(f"pole prostokąta wynosi: {pr.policzpole():.2f} cm2")
 
 tr = Trojkat(4.6,6.2)
 print(f"pole trójkąta wynosi: {tr.policzpole():.2f} cm2")
+
+trp = Trapez(8.5,6.1,4.8)
+print(f"pole trapezu wynosi: {trp.policzpole():.2f} cm2")
