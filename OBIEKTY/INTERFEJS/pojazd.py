059ef81dd9883cffd8907464dfb1136428eaff7a
albim72 <marcin.albiniak72@gmail.com>
2022-09-09 11:52:53 +0200

    Create pojazd.py

---
+++ OBIEKTY/INTERFEJS/pojazd.py
@@ -0,0 +1,9 @@
+from IPojazd import IPojazd
+
+class Pojazd(IPojazd):
+
+    def spalanie(self, odl, litry):
+        return litry*100/odl
+
+    def kosztyprzejazdu(self, odl, litry, cena_l):
+        return self.spalanie(odl,litry)*odl/100*cena_l
