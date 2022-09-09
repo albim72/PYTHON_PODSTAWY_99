c1db504efba975c643f9fe274fead80345880977
albim72 <marcin.albiniak72@gmail.com>
2022-09-09 11:41:49 +0200

    Update main.py

--- OBIEKTY/FIGURY/main.py
+++ OBIEKTY/FIGURY/main.py
@@ -1,6 +1,7 @@
 from prostokat import Prostokat
 from trojkat import Trojkat
 from trapez import Trapez
+from kolo import Kolo
 
 pr = Prostokat(4.5,6.3)
 print(f"pole prostokąta wynosi: {pr.policzpole():.2f} cm2")
@@ -10,3 +11,6 @@ print(f"pole trójkąta wynosi: {tr.policzpole():.2f} cm2")
 
 trp = Trapez(8.5,6.1,4.8)
 print(f"pole trapezu wynosi: {trp.policzpole():.2f} cm2")
+
+kl = Kolo(5.5)
+print(f"pole koła wynosi: {kl.policzpole():.2f} cm2")
