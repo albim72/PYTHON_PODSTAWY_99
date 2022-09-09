35f6f5b83ea62570284cbd858c93af975b763a0a
albim72 <marcin.albiniak72@gmail.com>
2022-09-09 12:18:11 +0200

    Update pojazd.py

--- OBIEKTY/INTERFEJS/pojazd.py
+++ OBIEKTY/INTERFEJS/pojazd.py
@@ -1,9 +1,27 @@
 from IPojazd import IPojazd
+from idane import IDane
 
-class Pojazd(IPojazd):
+class Pojazd(IPojazd,IDane):
+
+    def __init__(self,marka,model,rocznik,przebieg,rodz_silnika,poj,moc):
+        self.marka = marka
+        self.model = model
+        self.rocznik = rocznik
+        self.przebieg = przebieg
+        self.rodz_silnika = rodz_silnika
+        self.poj = poj
+        self.moc = moc
 
     def spalanie(self, odl, litry):
         return litry*100/odl
 
     def kosztyprzejazdu(self, odl, litry, cena_l):
         return self.spalanie(odl,litry)*odl/100*cena_l
+
+    def dane_pojazdu(self):
+        return f"Pojazd -> marka: {self.marka}, model: {self.model}, rocznik: {self.rocznik}, " \
+               f"przebieg: {self.przebieg}."
+
+    def dane_silnika(self):
+        return f"Silnik -> rodzaj silnika: {self.rodz_silnika}, pojemność: {self.poj} l, moc: {self.moc} kM."
+
