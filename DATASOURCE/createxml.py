48d949411f1c111c79147e10708db66d58b7428c
albim72 <marcin.albiniak72@gmail.com>
2022-09-12 11:12:30 +0200

    Create createxml.py

---
+++ DATASOURCE/createxml.py
@@ -0,0 +1,69 @@
+from xml.etree.ElementTree import Element,SubElement,Comment
+import xml.etree.ElementTree as ET
+from prettyfy import pretty
+
+top = Element("autokomis")
+sam1 = SubElement(top,'samochod')
+top.insert(0,Comment("użuwane egzemlarze samochodów marki Subaru...."))
+id = SubElement(sam1,'id')
+id.text = 'sam1'
+
+marka = SubElement(sam1,'marka')
+marka.text = 'Subaru'
+
+model = SubElement(sam1,'model')
+model.text = 'Impreza'
+
+poj = SubElement(sam1,'pojemnosc')
+poj.text = '2.0'
+
+rocznik = SubElement(sam1,'rocznik')
+rocznik.text = '1999'
+
+cena = SubElement(sam1,'cena')
+cena.text = '57600'
+
+wyp_dod = SubElement(sam1,'wyp_dodatkowe')
+
+kolor = SubElement(wyp_dod,'kolor')
+kolor.text = 'czarna perła'
+
+pod = SubElement(wyp_dod,'dodatkowe_pod')
+pod.text = '4'
+
+
+#drugi egzemplarz
+
+sam2 = SubElement(top,'samochod')
+
+id = SubElement(sam2,'id')
+id.text = 'sam2'
+
+marka = SubElement(sam2,'marka')
+marka.text = 'Subaru'
+
+model = SubElement(sam2,'model')
+model.text = 'Outback'
+
+poj = SubElement(sam2,'pojemnosc')
+poj.text = '2.4'
+
+rocznik = SubElement(sam2,'rocznik')
+rocznik.text = '2018'
+
+cena = SubElement(sam2,'cena')
+cena.text = '124000'
+
+wyp_dod = SubElement(sam2,'wyp_dodatkowe')
+
+kolor = SubElement(wyp_dod,'kolor')
+kolor.text = 'czerwony metallic'
+
+klima = SubElement(wyp_dod,'klimatyzacja')
+klima.text = 'ER5643'
+
+print(pretty(top))
+
+zapis_do_pliku = open("autokomis.xml","a",encoding="utf-8")
+zapis_do_pliku.write(pretty(top))
+zapis_do_pliku.close()
